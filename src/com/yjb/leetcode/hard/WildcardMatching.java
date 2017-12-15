af6caea34977417b1824ef0c2ef9476d4a4c937b
yangjiabing <yangjiabing1994@outlook.com>
2017-12-15 17:18:11 +0800

    Rearrange MedianOfTwoSortedArrays. Add WildcardMatching.

---
+++ src/com/yjb/leetcode/hard/WildcardMatching.java
@@ -0,0 +1,62 @@
+package com.yjb.leetcode.hard;
+
+/**
+ * 44. Wildcard Matching
+ * <p>
+ * Implement wildcard pattern matching with support for '?' and '*'.
+ * <p>
+ * '?' Matches any single character.
+ * '*' Matches any sequence of characters (including the empty sequence).
+ * <p>
+ * The matching should cover the entire input string (not partial).
+ * <p>
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ * <p>
+ * Some examples:
+ * isMatch("aa","a") → false
+ * isMatch("aa","aa") → true
+ * isMatch("aaa","aa") → false
+ * isMatch("aa", "*") → true
+ * isMatch("aa", "a*") → true
+ * isMatch("ab", "?*") → true
+ * isMatch("aab", "c*a*b") → false
+ */
+public class WildcardMatching {
+
+    public static void main(String[] args) {
+        String s = "aab";
+        String p = "*ab";
+        System.out.println(new WildcardMatching().isMatch(s, p));
+    }
+
+    public boolean isMatch(String s, String p) {
+        int i = 0;
+        int j = 0;
+        int starIndex = -1;
+        int iIndex = -1;
+
+        while (i < s.length()) {
+            if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+                ++i;
+                ++j;
+            } else if (j < p.length() && p.charAt(j) == '*') {
+                starIndex = j;
+                iIndex = i;
+                j++;
+            } else if (starIndex != -1) {
+                j = starIndex + 1;
+                i = iIndex + 1;
+                iIndex++;
+            } else {
+                return false;
+            }
+        }
+
+        while (j < p.length() && p.charAt(j) == '*') {
+            ++j;
+        }
+
+        return j == p.length();
+    }
+}
